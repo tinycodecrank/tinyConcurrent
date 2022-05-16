d4051d03949481164d8d1fa92538c26ffaad0f07
tinycodecrank <tinycodecrank@gmail.com>
2022-05-16 11:59:46 +0200

    update to jdk 17

---
+++ src/module-info.java
@@ -0,0 +1,10 @@
+module Concurrent
+{
+	exports de.tinycodecrank.concurrent.defer;
+	exports de.tinycodecrank.concurrent.yield;
+	exports de.tinycodecrank.concurrent;
+
+	requires Collections;
+	requires Functional;
+	requires Monads;
+}
